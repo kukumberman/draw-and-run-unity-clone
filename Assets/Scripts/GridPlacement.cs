f7cf0221b2dc43f6071b2a9faae35d0953b6e157
familiarstr4ng3r <strongbaby1337@gmail.com>
2021-08-23 19:13:33 +0300

    Update GridPlacement.cs

--- Assets/Scripts/GridPlacement.cs
+++ Assets/Scripts/GridPlacement.cs
@@ -7,7 +7,10 @@ public class GridPlacement : MonoBehaviour
     [SerializeField] private Vector2Int m_GridSize = Vector2Int.one;
     [SerializeField] private float m_ChunkSize = 1;
     [SerializeField] private float m_Spacing = 1;
-    //[SerializeField] private Vector3 m_Normal = Vector3.forward;
+    [SerializeField] private Vector3 m_Normal = Vector3.up;
+    [Space]
+    [SerializeField] private bool m_DrawGizmo = true;
+    [SerializeField] private bool m_UseUpAsNormal = false;
 
     public Vector3 TotalSize()
     {
@@ -26,8 +29,7 @@ public class GridPlacement : MonoBehaviour
         {
             for (int x = 0; x < m_GridSize.x; x++)
             {
-                Vector3 pos = GetPosition(x, y);
-                (pos.y, pos.z) = (pos.z, pos.y);
+                Vector3 pos = EvaluateNormal(GetPosition(x, y));
 
                 int index = y * m_GridSize.x + x;
                 positions[index] = pos;
@@ -52,20 +54,50 @@ public class GridPlacement : MonoBehaviour
         return position;
     }
 
+    private Vector3 EvaluateNormal(Vector3 xy)
+    {
+        return Quaternion.FromToRotation(Vector3.forward, m_Normal) * xy;
+    }
+
+    private Vector3[] GetCorners(Vector2 xy)
+    {
+        Vector3[] corners = new Vector3[4];
+
+        Vector2 halfSize = Vector2.one * m_ChunkSize * 0.5f;
+
+        corners[0] = EvaluateNormal(xy + new Vector2(-halfSize.x, -halfSize.y));
+        corners[1] = EvaluateNormal(xy + new Vector2(-halfSize.x, halfSize.y));
+        corners[2] = EvaluateNormal(xy + new Vector2(halfSize.x, halfSize.y));
+        corners[3] = EvaluateNormal(xy + new Vector2(halfSize.x, -halfSize.y));
+
+        return corners;
+    }
+
     private void OnDrawGizmos()
     {
-        Vector3 size = Vector3.one * m_ChunkSize;
-        size.y = 0;
+        if (!m_DrawGizmo) return;
+
+        if (m_UseUpAsNormal)
+        {
+            m_Normal = transform.up;
+        }
+
+        Vector3 origin = transform.position;
 
         for (int y = 0; y < m_GridSize.y; y++)
         {
             for (int x = 0; x < m_GridSize.x; x++)
             {
-                Vector3 pos = GetPosition(x, y);
-                //pos = Vector3.ProjectOnPlane(pos, m_Normal);
-                (pos.y, pos.z) = (pos.z, pos.y);
-                pos += transform.position;
-                Gizmos.DrawWireCube(pos, size);
+                Vector2 xy = GetPosition(x, y);
+
+                Vector3 pos = EvaluateNormal(xy);
+                Gizmos.DrawWireSphere(pos + origin, 0.01f);
+
+                var corners = GetCorners(xy);
+                for (int i = 0; i < corners.Length; i++)
+                {
+                    Gizmos.DrawLine(corners[i] + origin, corners[(i + 1) % corners.Length] + origin);
+                }
             }
         }
     }
